4780e3bf3c5b7012c15d73eb26d7b0a69811e6ee
neilnair <77185176+neilnair@users.noreply.github.com>
2021-05-09 13:27:10 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master (1)/project c31/Particle.js
@@ -0,0 +1,24 @@
+class Particle{
+    constructor(x,y,r){
+        var options = {
+            restitution = 2,
+            
+        }
+        this.r = r
+        this.body = Bodies.circle(x,y,this.r,options)
+        this.color=color(random(0,225),random(0,225),random(0,225));
+        World.add(world,this.body)
+    }
+    display(){
+      var pos = this.body.position
+      var angle = this.body.angle
+      push()
+      translate(pos.x,pos.y)
+      rotate(angle)
+      imageMode(CENTER)
+      fill(this.color)
+      ellipseMode(RADIUS)
+      ellipse(0,0,this.r,this,r)
+      pop()
+    }
+}
